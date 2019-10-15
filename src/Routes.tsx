bd9d062684e813a4d1cac0cf974c9c605d1bf975
Renato Perez <renatoperezc@gmail.com>
2019-10-15 14:52:13 -0600

    End ch4

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect
+  Redirect,
+  RouteComponentProps
 } from "react-router-dom";
-import AdminPage from "./AdminPage";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
 import ProductPage from "./ProductPage";
 import NotFoundPage from "./NotFoundPage";
 import LoginPage from "./LoginPage";
 
-const Routes: React.FC = () => {
-  const [loggedIn] = React.useState(true);
+const AdminPage = React.lazy(() => import("./AdminPage"));
+
+const RoutesWrap: React.FC = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Redirect exact from="/" to="/products" />
-          <Route exact path="/products" component={ProductsPage} />
-          <Route path="/products/:id" component={ProductPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route
-            path="/admin"
-            render={routeProps => {
-              return loggedIn ? (
-                <AdminPage {...routeProps} title="My Admmin Panel" />
-              ) : (
-                <Redirect to="/login" />
-              );
-            }}
-          />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <Route component={Routes} />
     </Router>
   );
 };
 
-export default Routes;
+const Routes: React.FC<RouteComponentProps> = ({ location }) => {
+  const [loggedIn] = React.useState(true);
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition key={location.key} timeout={200} classNames="animate">
+          <Switch>
+            <Redirect exact from="/" to="/products" />
+            <Route exact path="/products" component={ProductsPage} />
+            <Route path="/products/:id" component={ProductPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route
+              path="/admin"
+              render={routeProps => {
+                return loggedIn ? (
+                  <Suspense
+                    fallback={<div className="page-container">Loding...</div>}
+                  >
+                    <AdminPage {...routeProps} title="My Admmin Panel" />
+                  </Suspense>
+                ) : (
+                  <Redirect to="/login" />
+                );
+              }}
+            />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default RoutesWrap;
